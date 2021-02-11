f7249d80569892f8a76ee2832a09407f42575d49
Jon Cook <cookj@us.ibm.com>
2021-02-11 07:10:53 -0600

    funcSize1

--- src/size.js
+++ src/size.js
@@ -4,73 +4,58 @@ import * as Util from './util';
 import 'carbon-components/css/carbon-components.min.css';
 import { Dropdown } from 'carbon-components-react';
 
-class Size extends React.Component {
+function Size(props) {
 
-  constructor(props) {
-    
-    super(props);
-    this.state = {};
-  }
+  console.log('Size : enter');
+
+  console.log('Size : props : '+JSON.stringify(props));
+
+  let selectedSize = props.selected;
   
-  render() {
-    console.log('Size : render : enter');
+  if (selectedSize == null || selectedSize.length === 0) {
+    selectedSize = 'Select a size ...';
+  } 
 
-    console.log('Size : render : props : '+JSON.stringify(this.props));
-
-    let selectedSize = this.props.selected;
-    
-    if (selectedSize == null || selectedSize.length === 0) {
-      selectedSize = 'Select a size ...';
-    } 
-
-    let sizes = [];
-    // sizes.push(Util.SIZE_TITLE);
-    for (let i=0; i<Util.SIZES_ALLOWED.length; i++) {
-      sizes.push(Util.SIZES_ALLOWED[i]);
-    }
-
-    const items = [];
-    let selectedItem = null;
-    for (let i=0; i<sizes.length; i++) {
-      let size = sizes[i];
-      let item = {
-        id: 'size-'+i,
-        text: size,
-        value: size,
-      }
-
-      if (size === selectedSize) {
-        selectedItem = item;
-      }
-
-      items.push(item);
-    }
-
-    return (
-      <div className="cw-dropdown-container"
-      // debug
-      // style={ {  
-      //   // "height": 80, 
-      //    // "background-color": "pink" 
-      //  } }
-      >
-        <Dropdown
-          name="cw-sizes" 
-          id="cw-sizes"     
-          className="cw-init-select"    
-          titleText="Size"    
-          label={selectedSize}  
-          selectedItem={selectedItem}
-          items={items}
-          itemToString={(item) => (item ? item.text : '')}
-          size="sm"
-          onChange={(ev) => this.props.onChange(ev)}
-        />
-      </div>
-    ); 
-    
+  let sizes = [];
+  for (let i=0; i<Util.SIZES_ALLOWED.length; i++) {
+    sizes.push(Util.SIZES_ALLOWED[i]);
   }
 
+  const items = [];
+  let selectedItem = null;
+  for (let i=0; i<sizes.length; i++) {
+    let size = sizes[i];
+    let item = {
+      id: 'size-'+i,
+      text: size,
+      value: size,
+    }
+
+    if (size === selectedSize) {
+      selectedItem = item;
+    }
+
+    items.push(item);
+  }
+
+  return (
+    <div className="cw-dropdown-container"
+    >
+      <Dropdown
+        name="cw-sizes" 
+        id="cw-sizes"     
+        className="cw-init-select"    
+        titleText="Size"    
+        label={selectedSize}  
+        selectedItem={selectedItem}
+        items={items}
+        itemToString={(item) => (item ? item.text : '')}
+        size="sm"
+        onChange={(ev) => props.onChange(ev)}
+      />
+    </div>
+  ); 
+  
 }
 
 export default Size;
